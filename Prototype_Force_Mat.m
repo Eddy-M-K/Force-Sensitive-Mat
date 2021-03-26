d6b257bb81bc6a15011a31cd2c1ea9511b3c5945
Eddy-M-K <Eddy.M.K@outlook.com>
2021-03-26 19:56:34 -0400

    Minor changes

--- Prototype_Force_Mat.m
+++ Prototype_Force_Mat.m
@@ -4,7 +4,7 @@ close all
 clear vars
 format default
 
-% SETUP
+%SETUP
 %====================================
 
 a = arduino('COM4', 'Uno');
@@ -31,37 +31,31 @@ configurePin(a, zInput, 'AnalogInput');
 matrix = zeros(8,8);
 row = zeros(1,8);
 
-% fprintf("1.\t2.\t3.\t4.\t5.\t6.\t7.\t8.\t9.\t10.\t11.\t12.\t13.\t14.\t15.\t16.\t17.\t18.\t19.\t20.\t21.\t22.\t23.\t24.\t25.\t26.\t27.\t28.\t29.\t30.\t31.\t32.\t33.\t34.\t35.\t36.\t37.\t38.\t39.\t40.\t41.\t42.\t43.\t44.\t45.\t46.\t47.\t48.\t49.\t50.\t51.\t52.\t53.\t54.\t55.\t56.\t57.\t58.\t59.\t60.\t61.\t62.\t63.\t64.")
-% fprintf("\n---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\t---\n")
-
 % LOOP
 %====================================
 
 while (true) 
-    for pin=0:7
+    for row=0:7
         for i=0:2
-            if bitand(pin, bitshift(1,i))
+            if bitand(row, bitshift(1,i))
                 writeDigitalPin(a, Mux1{i+1}, 1);
             else
                 writeDigitalPin(a, Mux1{i+1}, 0);
             end
         end        
     
-        for pin2=0:7
+        for column=0:7
             for j=0:2
-                if bitand(pin2,bitshift(1,j))
+                if bitand(column,bitshift(1,j))
                     writeDigitalPin(a, Mux2{j+1}, 1);
                 else
                     writeDigitalPin(a, Mux2{j+1}, 0);
                 end
             end
             inputValue = readVoltage(a, 'A0');
-%             fprintf("%d\t", inputValue);
-            row(1,pin2+1) = inputValue;
+            matrix(8-row,column+1) = inputValue;
+            h = heatmap(matrix, 'ColorLimits',[0 5], 'XLabel', '2nd Multiplexer Wires', 'CellLabelColor','none', 'Title', 'Prototype Force Mat');
+            drawnow
         end
-        matrix(8-pin,:) = row;
     end
-%     fprintf("\n");
-    h = heatmap(matrix, 'ColorLimits',[0 5], 'XLabel', '2nd Multiplexer Wires', 'CellLabelColor','none', 'Title', 'Prototype Force Mat');
-    drawnow
 end
